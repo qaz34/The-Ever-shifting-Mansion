72d512281bcd93855ec36476020d9ab4663d25ca
Isaac Bowes <isaac.bowes@hotmail.com>
2017-09-13 16:53:46 +0930

    inventory started and ui looking clearer

---
+++ The Ever-Shifting Mansion/Assets/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject slot1;
+    public GameObject slot2;
+    public GameObject slot3;
+
+
+    void start()
+    {
+        slot1 = GameObject.Find("WeaponSlot1");
+        slot2 = GameObject.Find("WeaponSlot2");
+        slot3 = GameObject.Find("WeaponSlot3");
+    }
+
+
+}
