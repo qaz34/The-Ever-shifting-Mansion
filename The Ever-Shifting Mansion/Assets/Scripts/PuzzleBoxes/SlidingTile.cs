4deda06195d57fc7714b5a3df4b28af8319d5851
Connor Molseed <toothbrush112233@gmail.com>
2017-10-11 11:30:37 +1030

    -Added LoadingScene -Added ConnorsTestScene -Started work on tile puzzle

---
+++ The Ever-Shifting Mansion/Assets/Scripts/PuzzleBoxes/SlidingTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PuzzleBox
+{
+	public class SlidingTile : MonoBehaviour
+	{
+		[SerializeField]
+		SpriteRenderer tileImage;
+
+		public void SetImage(Sprite newImage)
+		{
+			tileImage.sprite = newImage;
+		}
+	}
+}
