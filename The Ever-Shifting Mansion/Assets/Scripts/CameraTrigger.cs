604174661078f5e98331216f1f9fa1d132eae2c4
Cameron Oborn <cameron.oborn@gmail.com>
2017-11-09 11:24:38 +1030

    fixed player controller added item glimmer

--- The Ever-Shifting Mansion/Assets/Scripts/CameraTrigger.cs
+++ The Ever-Shifting Mansion/Assets/Scripts/CameraTrigger.cs
@@ -14,25 +14,51 @@ public class CameraTrigger : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            connectedCamera.gameObject.SetActive(true);
-            if (!player.PreviousCamera)
-                player.PreviousCamera = player.currentCamera;
-            if (player.currentlyInCamera)
+            player.amIn.Add(this);
+            if (!player.currentCamera)
             {
-                player.currentlyInCamera.GetComponent<AudioListener>().enabled = false;
-                player.PreviousCamera.depth = 0;
+                player.currentCamera = connectedCamera;
+                connectedCamera.gameObject.SetActive(true);
             }
-            player.currentCamera = connectedCamera;
-            player.currentCamera.depth = 10;
-            player.currentCamera.GetComponent<AudioListener>().enabled = true;
-            player.currentlyInCamera = connectedCamera;
+
+            //connectedCamera.gameObject.SetActive(true);
+
+
+            //if (!player.PreviousCamera)
+            //{
+            //    player.PreviousCamera = player.currentCamera;
+            //}
+
+            //if (player.currentlyInCamera)
+            //{
+            //    player.currentlyInCamera.GetComponent<AudioListener>().enabled = false;
+            //    player.PreviousCamera.depth = 0;
+            //}
+
+            //player.currentCamera = connectedCamera;
+            //player.currentCamera.depth = 10;
+            //player.currentCamera.GetComponent<AudioListener>().enabled = true;
+            //player.currentlyInCamera = connectedCamera;
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
+            player.amIn.Remove(this);
+
+            player.currentCamera = player.amIn[player.amIn.Count - 1].connectedCamera;
+            if (!player.PreviousCamera)
+                player.PreviousCamera = connectedCamera;
+
             connectedCamera.gameObject.SetActive(false);
+            player.amIn[player.amIn.Count - 1].connectedCamera.gameObject.SetActive(true);
+            //if (player.currentlyInCamera == connectedCamera)
+            //{
+            //    player.PreviousCamera = 
+            //}           
+            //connectedCamera.gameObject.SetActive(false);
+
         }
     }
 }
